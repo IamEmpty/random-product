c505faf026f897b001d52975bb8d5128236c3d62
IamEmpty <evolution.is.already.underway@gmail.com>
2016-09-11 16:46:40 +0300

    tour become

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,18 @@
-module.exports = require('./webpack.dev.js');
+// module.exports = require('./config/webpack.dev.js');
+
+module.exports = {
+   entry: {
+     vendor: './app/vendor.ts',
+     app: './app/main.ts'
+   },
+   output: {
+     path: './dist',
+     filename: 'app.bundle.js'
+   },
+   loaders: [
+    {
+      test: /\.ts$/,
+      loaders: 'ts'
+    }
+  ]
+};
